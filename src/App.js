3c547be04a00ad6c3ff779ccf1b9b1e90461758f
Matheus Cruz <mccruz2611@gmail.com>
2023-02-02 00:19:17 -0300

    formatando a section

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import styled from 'styled-components';
 const AppConteiner = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(90deg, #002f52 35%, #326589);
-  font-family: Arial, Helvetica, sans-serif;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
 `;
 
 function App() {
