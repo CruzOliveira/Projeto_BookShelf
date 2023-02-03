d74249ec5e9843d6dbc7dadec16ebcae2286b466
Matheus Cruz <mccruz2611@gmail.com>
2023-02-03 15:05:48 -0300

    arrumando

--- src/components/Lancamentos/index.js
+++ src/components/Lancamentos/index.js
@@ -1,16 +1,3 @@
-
-import React from "react";
-import styled from "styled-components";
-import { livros } from "../Pesquisa/dadosPesquisa";
-import { Titulo } from "../Titulo/index.js";
-
-const LancamentoConteiner = styled.section`
-    display: flex;
-    flex-direction: column;
-    background-color: #ebecee;
-    overflow-x: hidden;
-    height: 50vh;
-
 import React from 'react';
 import styled from 'styled-components';
 import { livros } from '../Pesquisa/dadosPesquisa';
@@ -23,50 +10,48 @@ const LancamentoConteiner = styled.section`
   flex-direction: column;
   background-color: #ebecee;
   overflow-x: hidden;
-  height: 100vh;
+  height: 50vh;
 `;
-
 const NovosLivrosConteiner = styled.div`
-    margin-top: 30px;
-    display: flex;
-    overflow-x: hidden;
-    justify-content: center;
+  margin-top: 30px;
+  display: flex;
+  overflow-x: hidden;
+  justify-content: center;
 `;
 const Seletor = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 80vw;
-    overflow-x: scroll;
+  display: flex;
+  justify-content: space-between;
+  width: 80vw;
+  overflow-x: scroll;
 
-    img {
-        width: 209px;
-        height: 282px;
-        margin: 20px 20px;
-        cursor: pointer;
-        box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-        &:hover {
-            border: 2px solid white;
-        }
+  img {
+    width: 209px;
+    height: 282px;
+    margin: 20px 20px;
+    cursor: pointer;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+    &:hover {
+      border: 2px solid white;
     }
+  }
 `;
 
 function Lancamentos() {
+  return (
+    <LancamentoConteiner>
+      <Titulo cor={'#EB9B00'} tamanhoFonte={'36px'}>
+        ÚLTIMOS LANÇAMENTOS
+      </Titulo>
+      <NovosLivrosConteiner>
+        <Seletor>
+          {livros.map((livro) => (
+            <img src={livro.src} />
+          ))}
+        </Seletor>
+      </NovosLivrosConteiner>
+    </LancamentoConteiner>
+  );
 
-    return (
-        <LancamentoConteiner>
-            <Titulo cor={"#EB9B00"} tamanhoFonte={"36px"}>
-                ÚLTIMOS LANÇAMENTOS
-            </Titulo>
-            <NovosLivrosConteiner>
-                <Seletor>
-                    {livros.map((livro) => (
-                        <img src={livro.src} />
-                    ))}
-                </Seletor>
-            </NovosLivrosConteiner>
-        </LancamentoConteiner>
-    );
-  
   return (
     <LancamentoConteiner>
       <Titulo cor={'#EB9B00'} tamanhoFonte={'36px'}>
@@ -87,7 +72,6 @@ function Lancamentos() {
       />
     </LancamentoConteiner>
   );
-
 }
 
 export default Lancamentos;
